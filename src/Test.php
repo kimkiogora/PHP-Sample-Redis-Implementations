71f02a0eacfc665cda17c26d78f05974ce826779
Kim Kiogora <kimkiogora@gmail.com>
2015-05-05 16:50:33 +0300

    Update Test.php

--- src/Test.php
+++ src/Test.php
@@ -1,2 +1,23 @@
 <?php
-?>
+require 'RedisLib.php';
+
+$memcache = new MemcacheLib(30);
+
+//Set cache first
+//$red = $memcache->saveToCache('stuff', 
+//        json_encode(array('username'=>'kimkiogora@gmail.com',
+//    'password'=>'!23qweASD'))
+//       );
+//print_r($red);
+
+//echo $memcache->retrieveFromCache("stuff");
+//echo "\n";
+//echo $memcache->retrieveFromCache("key");
+
+
+$res = $memcache->retrieveFromCache("stuff");
+if($res == FALSE ) {
+    echo "Memcache Results: $res";
+} else {
+    print_r($res);
+}
