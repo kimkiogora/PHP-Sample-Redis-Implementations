a291cddfd6e6a96bb475d36453b9ea0156bccc01
Kim Kiogora <kimkiogora@gmail.com>
2015-05-05 16:45:31 +0300

    Create RedisLib.php

---
+++ src/RedisLib.php
@@ -0,0 +1,19 @@
+<?php
+/**
+  * @uthor: kimkiogora@gmail.com
+  * @usage: Redis Functionality
+*/
+class RedisLib {
+  /*
+   * Variable timeout, 1 DAY
+  */
+  $timeout=24*3600;
+  
+  
+  public static function __setData($key,$value,$timeout=null){
+  }
+  
+  public static function __getData($key){
+  }
+}
+?>
